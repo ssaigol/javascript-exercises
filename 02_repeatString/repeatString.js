7fbce6ad79c15077b1bfc1ed7dc48172f0b50436
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2024-10-08 18:08:59 -0400

    Complete javascript exercise #02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let a = "";
+    for (let i = 0; i < num; i++) {
+        a += string;
+    }
+    return a;
 };
 
 // Do not edit below this line
