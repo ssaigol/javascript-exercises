6c50f72806d183c8e72eff08ae6744b4a9e4442c
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2024-10-09 16:57:34 -0400

    Complete javascript exercise #07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let a = ((temp - 32)*(5/9));
+    return Math.round(a*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let b = (temp * (9/5) + 32);
+  return Math.round(b*10)/10;
 };
 
 // Do not edit below this line
