93b0114ec2893a3ac37bf59e74583f5e539f8ad4
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2024-10-09 15:58:31 -0400

    Complete javascript exercise #03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let string = inputString;
+    let length = string.length;
+    let stringArray = [];
+    for (let i = 0; i < length; i++) {
+        stringArray[i] = string.charAt(i);
+    }
+    let reverseArray = [];
+    for (let u = 0; u < length; u++) {
+        reverseArray[u] = stringArray[length - (u + 1)];
+    }
+    let outputString = reverseArray.join("");
+    return outputString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
+
