fb3655353effbd347f44931a31207d0ba07d1c3e
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2024-10-30 18:04:12 -0400

    Complete javascript exercise #10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+index = Number(input);
+if (index === 0) return 0;
+if (index < 0) return "OOPS";
+const sequence = [1, 1];
+for (let i = 2; i < index + 1; i++) {
+    sequence[i] = sequence[i-2] + sequence[i-1];
+}
+return sequence[index-1];
 };
 
 // Do not edit below this line
