5cd7cd4e27b7c0e8a666af070cca2a373121d25c
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2024-10-30 18:04:40 -0400

    Complete javascript exercise #09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const arr = string.split('');
+    const forwardArr = arr.filter(char => char != " " && char != "," && char != "." && char != "!");
+    const backwardsArr = forwardArr.toReversed();
+    var isPalindrome = false;
+    if (forwardArr.join("").toLowerCase() === backwardsArr.join("").toLowerCase()) {
+        return true;
+    } else return false;
+ };
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
