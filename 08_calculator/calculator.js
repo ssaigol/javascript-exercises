983cd5ff93bf3ab42c5c4ed8770db13bea1290ac
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2024-10-30 17:12:11 -0400

    Complete javascript exercise #08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total * num, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let total = a;
+  if (b > 1) {
+    for (let i = 1; i < b; i++) {
+      total = total * a;
+    }
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a < 2) return 1;
+  let total = a;
+  for (let i = a - 1; i > 0; i--) {
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
