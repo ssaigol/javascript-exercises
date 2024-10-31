57edbc54b2ba7471e9ce6861879629cd45fe3d17
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2024-10-31 14:11:29 -0400

    Complete javascript exercise #12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const CURRENTYEAR = new Date().getFullYear();
+    const noDeathYear = array.map(function(obj) {
+        if (!("yearOfDeath" in obj)) {
+            obj.yearOfDeath = CURRENTYEAR;
+        }
+    })
+    const ages = array.map(obj => obj.age = obj.yearOfDeath - obj.yearOfBirth)
+    const sorted = array.sort(function(a, b){
+        if (a.age > b.age) {
+            return -1;
+        } else if (a.age < b.age) {
+            return 1;
+        } else return 0;
+    })
+    return array[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
